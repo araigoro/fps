fe7c24cb5868dbf1fdf4800207b0117cb8021026
araigoro <kaito1019baseball@icloud.com>
2021-05-31 22:59:38 +0900

    ・初期設定 ・銃のアセット追加

---
+++ FPScso/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject[] gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gun[1] = transform.Find("Handgun_01_FPSController").gameObject;
+        gun[2]= transform.Find("Assault_Rifle_01_FPSController").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            gun[1].SetActive(true);
+            gun[2].SetActive(false);
+        }
+        else if(Input.GetKey(KeyCode.Alpha2))
+        {
+            gun[1].SetActive(false);
+            gun[2].SetActive(true);
+        }
+    }
+}
